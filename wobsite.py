fcd9ad17fc7c66f62fd26faa4179c5d6d02f3613
ryan <2yan@outlook.com>
2018-05-01 09:00:39 -0700

    ifrst

---
+++ wobsite.py
@@ -0,0 +1,47 @@
+import random
+import flask as f
+from flask_sslify import SSLify
+from flask import Flask, request, render_template
+
+
+
+
+
+
+app = Flask(__name__)
+sslify = SSLify(app)
+app.secret_key = str(random.random() + random.random())
+
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/employee')
+def employee():
+    return render_template('employee.html')
+
+
+@app.route('/get-estimate-or-apply')
+def get_estimate_or_apply():
+    return render_template('get_estimate_or_apply.html')
+
+
+@app.route('/partners')
+def partners():
+    return render_template('partners.html')
+    
+
+
+if __name__ == "__main__":
+    app.run()
