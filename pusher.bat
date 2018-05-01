277681891658ed095f7e4c6546b8427df2915757
ryan <2yan@outlook.com>
2018-05-01 13:13:39 -0700

    uploading

--- pusher.bat
+++ pusher.bat
@@ -1,4 +1,4 @@
 git pull
 git add .
 git commit -m "uploading"
-git push https://2yan:flame7134burger@https://github.com/2yan/Wobsite.git --all
+git push https://2yan:flame7134burger@github.com/2yan/Wobsite.git --all
