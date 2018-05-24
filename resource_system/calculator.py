8c7d6f65bdc2903c58fde41defd65af24fe19854
ryan <2yan@outlook.com>
2018-05-24 12:38:54 -0700

    accurate calculator

--- resource_system/calculator.py
+++ resource_system/calculator.py
@@ -1,4 +1,5 @@
 import flask as f
+import numpy as np
 
 def compound_interest_calculator():
     page_url = 'resource_system/compound_interest_calculator.html'
@@ -18,14 +19,39 @@ def cash_format(num):
     return '$ {:,.0f}'.format(num)
 
 
-def calculate(p, r, n, t):
-    p = float(p)
-    r = float(r)
-    n = float(n)
-    t = float(t)
-    a = p * (1 + r/n)**(n * t)
+def get_payment(i, a, n):
+    p = i * a / (1 - (1 + i) ** -n)
+    return p
 
-    cost = a - p
-    pmt_size = a/t
+
+def get_cost(periods, interest, initial, payment):
+    owed = initial
+    cost = 0
+    last_owed = owed
+
+    while owed > 0:
+
+        owed = owed * (1 + interest)
+        cost = cost + last_owed - owed
+
+        if payment > owed:
+            payment = owed
+        owed = owed - payment
+        if owed > last_owed:
+            return 'inf'
+        last_owed = owed
+
+    return cost
+
+
+def calculate(p, r, n, t):
+    periods = n * t
+    interest = r / n
+    initial = p
+    pmt_size = get_payment(interest, initial, periods)
+
+    cost = get_cost(periods, interest, initial, pmt_size)
 
     return cost, pmt_size
+
+
