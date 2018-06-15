54303fbcd00c98f145f22e0b3287c6ccd192da12
ryan <2yan@outlook.com>
2018-06-15 15:38:15 -0700

    test

--- bank_statement_system/plaid.py
+++ bank_statement_system/plaid.py
@@ -10,5 +10,9 @@ PLAID_SECRET = os.getenv('PLAID_SECRET')
 PLAID_PUBLIC_KEY = os.getenv('PLAID_PUBLIC_KEY')
 
 @app.route('/')
-def login():
+def home():
     return PLAID_CLIENT_ID
+
+@app.route('/test')
+def test():
+    return 'HELLO WORLD'
