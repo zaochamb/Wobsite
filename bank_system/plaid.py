7d1693c08feb04a311aa5fc535f3a1e6518aaecd
ryan <2yan@outlook.com>
2018-06-18 08:21:02 -0700

    added form to get name

--- bank_system/plaid.py
+++ bank_system/plaid.py
@@ -11,9 +11,17 @@ PLAID_PUBLIC_KEY = os.getenv('PLAID_PUBLIC_KEY')
 
 client = Client(client_id=PLAID_CLIENT_ID, secret=PLAID_SECRET, public_key=PLAID_PUBLIC_KEY, environment='sandbox')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return '''Form to get user info'''
+    page = 'bank_system/login_form.html'
+    if f.request.method == 'GET':
+        return f.render_template(page)
+
+    if f.request.method =='POST':
+        search_text = f.request.form['bank_name']
+        bank_suggestions = get_bank_suggestions(search_text)
+        return f.render_template(page, options = bank_suggestions )
 
 
-
+def get_bank_suggestions(text):
+    return ['option1', 'option2', text]
