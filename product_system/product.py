c4109ba2e57aaaa2177075b91aa94d02b4aea7f2
ryan <2yan@outlook.com>
2018-06-25 13:19:04 -0700

    added product and made selection box automatic

--- product_system/product.py
+++ product_system/product.py
@@ -22,7 +22,7 @@ def products(product=''):
     if product == 'small_business_line_of_credit':
         return f.render_template('product_system/' + product + '.html')
     prod_text = product.replace('_', ' ').title()
-    return f.redirect(f.url_for('contact', greeting = '{} - Lets Begin Your Application'.format(prod_text)))
+    return f.redirect(f.url_for('contact', greeting = '{} - Lets Begin Your Application'.format(prod_text), product = prod_text))
 
 
 def get_product_list():
